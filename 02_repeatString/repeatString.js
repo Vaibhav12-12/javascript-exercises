4a953314ef7dd071ea1ff0cd812ce6461eebba06
Vaibhav <b.vaibhav.0012@gmail.com>
2024-10-24 17:41:23 +0530

    Completed the Exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    let result = "";
+    while(times > 0) {
+        times--;
+        result += str;
+    }
+    return result;
 };
 
 // Do not edit below this line
