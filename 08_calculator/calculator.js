c9c16911377b8cd8ef92e70469a82f888968de3e
Vaibhav <b.vaibhav.0012@gmail.com>
2024-11-10 14:11:43 +0530

    Completed the Exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let result = 0;
+  for(let num of arr) {
+    result += num;
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 1;
+  for(let num of arr) {
+    result *= num;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	let result = 1;
+  while(pow > 0) {
+    pow--;
+    result *= num;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+	while(num > 0) {
+    result *= num;
+    num--;
+  }
+  return result;
 };
 
 // Do not edit below this line
