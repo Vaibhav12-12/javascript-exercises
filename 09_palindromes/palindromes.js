4838ae101df82a9f15598252cf1b4a7aee785494
Vaibhav <b.vaibhav.0012@gmail.com>
2024-11-10 15:32:50 +0530

    Completed the Exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let result = "";
+    for(let i = word.length - 1; i >= 0; i--) {
+        result += word[i];
+    }
+    return result === word;
 };
 
 // Do not edit below this line
