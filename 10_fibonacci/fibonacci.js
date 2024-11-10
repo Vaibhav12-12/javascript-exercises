6bcb9cfffb82255438879134812f4b6b8109f83f
Vaibhav <b.vaibhav.0012@gmail.com>
2024-11-10 20:10:45 +0530

    Finished the exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n < 0) return "OOPS";
+    n = Number(n);
+    let arr = [0, 1];
+    if(n <= 1) return arr[n];
+    for(let i = 2; i <= n; i++) {
+        arr.push(arr[i - 1] + arr[i - 2]);
+    }
+    return arr[n];
 };
 
 // Do not edit below this line
