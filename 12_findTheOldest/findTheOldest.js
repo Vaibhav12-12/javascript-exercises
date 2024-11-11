934140c886b734cb89f606ce3083a14d50829387
Vaibhav <b.vaibhav.0012@gmail.com>
2024-11-11 18:18:17 +0530

    Completed the task

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    
+    let oldest = people.reduce((acc, cur) => {
+      const accAge = (acc.yearOfDeath || currentYear) - acc.yearOfBirth;
+      const curAge = (cur.yearOfDeath || currentYear) - cur.yearOfBirth;
+      return curAge > accAge ? cur : acc;
+    });
+  
+    return oldest;
+  };
+  
 
 // Do not edit below this line
 module.exports = findTheOldest;
