38242cb7e8b192afc0a1fc2d8f57b976c829ccb9
Vaibhav <b.vaibhav.0012@gmail.com>
2024-11-11 17:18:09 +0530

    Completed the task

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = [];
+    for(let book of books) {
+        titles.push(book.title);
+    }
+    return titles;
+    // Can use map as well, titles = books.map(book => book.title);
 };
 
 // Do not edit below this line
